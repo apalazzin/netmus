77b519e466dbc36a936c1a6775478a8efdd13ea8
danyleleorti <danyleleorti@hotmail.com>
2011-02-13 12:06:16 +0100

    FIX - Implementati UserDTO e UserSummaryDTO, mentre UserCompleteDTO ? ancora incompleto

--- src/it/unipd/netmus/shared/UserDTO.java
+++ src/it/unipd/netmus/shared/UserDTO.java
@@ -9,5 +9,61 @@ package it.unipd.netmus.shared;
  */
 @SuppressWarnings("serial")
 public class UserDTO extends UserSummaryDTO {
+	
+	private String firstName;
+	private String lastName;
+	private String birth;
+	
+	//default constructor
+	public UserDTO(){
+		this(new String(), new String(), new String(),new String(), new String(), new String(),new String());
+	}
+	
+	public UserDTO(String nick,String mail,String sexType,String city){
+		this(nick,mail,sexType,city,new String(), new String(), new String());
+		
+	}
+	
+	public UserDTO(String name,String surname,String birthDate){
+		this(new String(), new String(), new String(),new String(), name,surname,birthDate);
+	}
+	
+	public UserDTO(String nick,String mail,String sexType,String city,String name,String surname,String birthDate){
+		super(nick,mail,sexType,city);
+		this.firstName = name;
+		this.lastName = surname;
+		this.birth = birthDate;
+	}
+	
+	
+	
+	public String getName()
+	{
+		return firstName;
+	}
+	
+	public void setFirstName(String name)
+	{
+		this.firstName = name;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public void setLastName(String surname)
+	{
+		this.lastName = surname;
+	}
 
+	public void setBirthDate(String birthDate)
+	{
+		this.birth = birthDate;
+	}
+	
+	public String getBirthDate()
+	{
+		return birth;
+	}
 }
--- src/it/unipd/netmus/shared/UserSummaryDTO.java
+++ src/it/unipd/netmus/shared/UserSummaryDTO.java
@@ -9,5 +9,61 @@ package it.unipd.netmus.shared;
  */
 @SuppressWarnings("serial")
 public class UserSummaryDTO implements GenericDTO {
-
+	private String nickName;
+	private String email;
+	private String sex;
+	private String location;
+	
+	//default constructor
+	public UserSummaryDTO(){
+		this(new String(), new String(), new String(), new String());
+	}
+	
+	public UserSummaryDTO(String nick,String mail,String sexType,String city){
+		this.nickName = nick;
+		this.email = mail;
+		this.sex = sexType;
+		this.location = city;
+	}
+	
+	public String getNickName()
+	{
+		return nickName;
+	}
+	
+	public void setNickName(String nick)
+	{
+		this.nickName = nick;
+	}
+	
+		
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public void setEmail(String mail)
+	{
+		this.email = mail;
+	}
+	
+	public String getSex()
+	{
+		return sex;
+	}
+	
+	public void setSex(String sexType)
+	{
+		this.sex = sexType;
+	}
+	
+	public String getLocation()
+	{
+		return location;
+	}
+	
+	public void setLocation(String city)
+	{
+		this.location = city;
+	}
 }
