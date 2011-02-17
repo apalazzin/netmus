1947738ca54511dbcf856ef13b0c140e7e171093
fede baron 89 <fede.baron.89@gmail.com>
2011-02-17 11:13:02 +0100

    FIX - UserAccount - Ripristino datastore funzionante, versione precendete alle cazzete che ho fatto stanotte!

--- src/it/unipd/netmus/shared/UserCompleteDTO.java
+++ src/it/unipd/netmus/shared/UserCompleteDTO.java
@@ -10,17 +10,45 @@ package it.unipd.netmus.shared;
 @SuppressWarnings("serial")
 public class UserCompleteDTO extends UserDTO {
 
-	private MusicLibraryDTO musicLibrary;
+	private String password;
+	private int nSongs;
+	
+	public UserCompleteDTO(){
+		super();
+		password = "";
+		nSongs = 0;
+	}
+	
+	public UserCompleteDTO(String nick,String mail,String sexType,String city,String name,String surname,String birthDate, String pass){
+		super(nick,mail,sexType,city,name,surname,birthDate);
+		password = pass;
+	}
+	
+	public UserCompleteDTO(String nick,String mail,String sexType,String city, String pass){
+		super(nick,mail,sexType,city);
+		password = pass;
+	}
 
-    public UserCompleteDTO() {
-        super();
-    }
-    
-    public MusicLibraryDTO getMusicLibrary() {
-        return musicLibrary;
-    }
+	public UserCompleteDTO(String name,String surname,String birthDate, String pass){
+		super(name,surname,birthDate);
+		password = pass;
+	}
+	
+	
+	public void setPassword(String p){
+		password = p;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
 
-    public void setMusicLibrary(MusicLibraryDTO musicLibrary) {
-        this.musicLibrary = musicLibrary;
-    }
+	
+	public void setNsongs(int n){
+		nSongs = n;
+	}
+	
+	public int getNsongs(){
+		return nSongs;
+	}
 }
