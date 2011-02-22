39af40a88c1db6ad832991a3d5ffcb576ceea75c
andrea mandolo <andrea.mandolo@gmail.com>
2011-02-22 16:39:41 +0100

    ADD - AppletBar.java - Inserita gestione On OFF del applet + cambiato jar applet

---
+++ src/it/unipd/netmus/client/applet/AppletConstants.java
@@ -0,0 +1,10 @@
+package it.unipd.netmus.client.applet;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface AppletConstants extends Constants {
+	String appletEnable();
+	String appletDisable();
+	String rescan();
+	String title();
+}
