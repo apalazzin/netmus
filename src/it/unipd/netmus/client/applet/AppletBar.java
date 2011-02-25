a1f07e47fd3ac81c4c07c002be6e0c0d2823c581
andrea mandolo <andrea.mandolo@gmail.com>
2011-02-25 17:46:37 +0100

    ADD,FIX - Sistemato package applet - Progettata Versione Modulare, divisa grafica, logica, comunicazione.

--- src/it/unipd/netmus/client/applet/AppletBar.java
+++ src/it/unipd/netmus/client/applet/AppletBar.java
@@ -7,30 +7,12 @@ import java.util.List;
 
 import it.unipd.netmus.client.ClientFactory;
 import it.unipd.netmus.client.event.DeviceScannedEvent;
-import it.unipd.netmus.client.place.ProfilePlace;
 import it.unipd.netmus.client.service.LibraryService;
 import it.unipd.netmus.client.service.LibraryServiceAsync;
 import it.unipd.netmus.shared.SongDTO;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseOverEvent;
-import com.google.gwt.event.dom.client.MouseOverHandler;
-import com.google.gwt.event.shared.GwtEvent;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.view.client.SelectionChangeEvent;
 
 /**
  * @author ValterTexasGroup
@@ -38,308 +20,104 @@ import com.google.gwt.view.client.SelectionChangeEvent;
  */
 public class AppletBar {
 	
-	private static AppletBar APPLET_BAR = null;
-    private boolean visible = false;
-    private AppletConstants constants = GWT.create(AppletConstants.class);
-    private Label title = new Label(constants.title());
-    private Label onOff = new Label();
-    private Button rescan = new Button(); // VISIBILE FINITA LA SCANSIONE
-    private HTMLPanel button_container = new HTMLPanel("");
-    private Button chooser = new Button();
-    private TextBox status = new TextBox();
-    private HTML applet = new HTML();
-    private String user;
-    private String original_user;
-    private boolean state;
-    private TranslateDTOXML translator = new TranslateDTOXML();
-    private HTMLPanel main = new HTMLPanel("");
-    ClientFactory client_factory = GWT.create(ClientFactory.class);
-    
+	static AppletBar APPLET_BAR = null; // oggetto istanza statico per SINGLETON
+    private boolean visible = false; // stato di visibilita' della barra
+    private String user; // identificativo utente loggato
+    private boolean state = true; // stato applet alla creazione
+    private ClientFactory client_factory = GWT.create(ClientFactory.class);
     private LibraryServiceAsync libraryService = GWT.create(LibraryService.class);
+    private static AppletConstants constants = GWT.create(AppletConstants.class);
     
-    public static AppletBar get(String user, boolean state) {
+    private TranslateDTOXML translator = new TranslateDTOXML();
+    
+    // getter per questa classe che utilizza il patter SINGLETON
+    public static AppletBar get(String user) {
         if (APPLET_BAR == null)
-            APPLET_BAR = new AppletBar(user, state);
-        APPLET_BAR.setUser(user);
+            APPLET_BAR = new AppletBar(user);
+        else
+            APPLET_BAR.setUser(user);
         return APPLET_BAR;
     }
     
+    // memorizzo username dell'utente loggato
     private void setUser(String user) {
         this.user = user;
     }
     
-    private AppletBar(String user, boolean state) {
-    	
-        this.original_user = user;
-    	this.user=user.replaceAll("@\\S*", "");
-    	this.state=state;
-        
-        makeNativeFunction(this);
-
-
-        RootPanel.get("applet-bar").setStyleName("applet-bar");
-
-        main.setStyleName("applet_main");
-        button_container.setStyleName("applet_button_cnt");
-        
-        onOff.setSize("50px","14px");
-        onOff.setStyleName("applet_switch");
-        
-        if (state) onOff.setText(constants.appletDisable());
-        else onOff.setText(constants.appletEnable());
-        
-        rescan.setStyleName("applet_rescan");
-        rescan.setText(constants.rescan());
-        rescan.getElement().getStyle().setMarginLeft(30, Style.Unit.PX);
-        rescan.getElement().getStyle().setWidth(90, Style.Unit.PX);
-        rescan.setVisible(false); // diventera' visibile a fine scansione di un dispositivo
-                
-        chooser.setText(constants.chooser());
-        chooser.setStyleName("applet_bottone");
-        chooser.getElement().getStyle().setMarginLeft(30, Style.Unit.PX);
-        chooser.getElement().getStyle().setWidth(90, Style.Unit.PX);
-        
-        title.setStyleName("applet_title");
-        status.addStyleName("applet_status");
-
-        RootPanel.get("applet-bar").addDomHandler(new MouseOverHandler() {
-
-            @Override
-            public void onMouseOver(MouseOverEvent event) {
-                if(DOM.getElementById("main_panel").getClientWidth()>805) {
-                    DOM.getElementById("main_panel").getStyle().setMarginRight(157, Style.Unit.PX);
-                    DOM.getElementById("applet-bar").getStyle().setWidth(140, Style.Unit.PX);
-                    status.getElement().getStyle().setProperty("MozTransform", "rotate(0deg)");
-                    status.getElement().getStyle().setProperty("WebkitTransform", "rotate(0deg)");
-                    status.getElement().getStyle().setProperty("Transform", "rotate(0deg)");
-                    status.getElement().getStyle().setMarginLeft(7, Style.Unit.PX);
-                    status.getElement().getStyle().setMarginTop(15, Style.Unit.PX);
-                    status.getElement().getStyle().setWidth(120, Style.Unit.PX);
-                    
-                    button_container.getElement().getStyle().setOpacity(1);
-                    title.getElement().getStyle().setOpacity(1);
-                } else {
-                    
-                    DOM.getElementById("applet-bar").getStyle().setBackgroundColor("#fabebe");
-                }
-            }}, MouseOverEvent.getType());
-        
-
-        RootPanel.get("applet-bar").addDomHandler(new MouseOutHandler() {
-
-            @Override
-            public void onMouseOut(MouseOutEvent event) {
-                DOM.getElementById("applet-bar").getStyle().setBackgroundColor("#FFFFFF");
-                DOM.getElementById("main_panel").getStyle().setMarginRight(50, Style.Unit.PX);
-                DOM.getElementById("applet-bar").getStyle().setWidth(33, Style.Unit.PX);
-                status.getElement().getStyle().setProperty("MozTransform", "rotate(90deg)");
-                status.getElement().getStyle().setProperty("WebkitTransform", "rotate(90deg)");
-                status.getElement().getStyle().setProperty("Transform", "rotate(90deg)");
-                status.getElement().getStyle().setMarginLeft(-86, Style.Unit.PX);
-                status.getElement().getStyle().setMarginTop(60, Style.Unit.PX);
-                status.getElement().getStyle().setWidth(200, Style.Unit.PX);
-                button_container.getElement().getStyle().setOpacity(0);
-                title.getElement().getStyle().setOpacity(0);
-            }}, MouseOutEvent.getType());
-
-
-        
-        VerticalPanel tmp = new VerticalPanel();
-        
-        tmp.add(rescan);
-        tmp.add(chooser);
-        
-        button_container.add(tmp);
-        main.add(title);
-        main.add(status);
-        
-        main.add(button_container);
-        main.add(onOff);    
-        main.add(applet);
-        RootPanel.get("applet-bar").add(main);
-        
-        
-        onOff.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                // send changeStatus signal
-                if(event.getRelativeElement().getStyle().getColor().equals("red")) {
-                    event.getRelativeElement().getStyle().setColor("#38D12F");
-                } else {
-                    event.getRelativeElement().getStyle().setColor("red");
-                }
-               
-                changeState();
-            }
-        });
-        
-        onOff.addMouseOverHandler(new MouseOverHandler() {
-
-            @Override
-            public void onMouseOver(MouseOverEvent event) {
-                event.getRelativeElement().getStyle().setCursor(Style.Cursor.POINTER);
-            }
-            
-        });
-        
-        rescan.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                // send rescan signal
-                reScanAll();
-            }
-        });
-        
-        chooser.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                // send rescan signal
-                showChooser();
-            }
-        });
+    // costruttore privato (SINGLETON)
+    private AppletBar(String user) {
+    	this.user=user;
+    	// pubblico i metodi nativi per renderli invocabili dall'applet
+        AppletConnector.makeNativeFunction(this);
+        // creo la view dell'applet
+        AppletBarView.createView();
     }
     
+    // carico l'applet e rendo visibile la barra
     public void appletBarON() {
         if (!visible) {
             visible = true;
-            RootPanel.get("applet-bar").setVisible(true);
-            applet.setHTML("<applet " +
-            		"style=\"position:relative; left:-99999px;\"" +
-            		"id='netmus_applet' name='netmus_applet' " +
-                    "code=\"applet/NetmusApplet.class\" " +
-                    "archive=\"applet/netmusApplet.jar, applet/jid3lib-0.5.4.jar\" " +
-                    "width=1 height=1></applet>");
-            
-            System.out.println("Applet caricata");
+            AppletBarView.showBar();
         }
     }
     
+    // spengo l'applet e rendo invisibile la barra
     public void appletBarOFF() {
         if (visible) {
             visible = false;
-            RootPanel.get("applet-bar").setVisible(false);
-            applet.setHTML("");
+            AppletBarView.hideBar();
         }
     }
     
-    // metodi per comunicare con l applet
     
-    // per dire all'applet di cambiare stato
-    // dopo che ONOFF e' stato premuto
-    private void changeState() {
+    // --- METODI PER LA LOGICA DI COMUNICAZIONE TRA VIEW E APPLET ---
+    
+    // metodo per far attivare/disattivare la ricerca automatica dei dispositivi
+    void changeState() {
         state = !state;
         if (state)
-            onOff.setText(constants.appletDisable());
+            AppletBarView.setOnOffButton(false);
         else
-            onOff.setText(constants.appletEnable());
+            AppletBarView.setOnOffButton(true);
+        AppletConnector.setState(state); // cambio dello stato dentro l'applet
+    }
+    
+    // avvisa applet di fare una scansione completa del dispositivo inserito
+    // ignorando il vecchio file di log che escludeva i file gia' scansionati
+    void reScanAll() {
+        AppletConnector.sendRescan();
+    }
+    
+    void showChooser() {
+        AppletConnector.showChooser();
+    }
+    
+    void sendStarts() {
+        AppletConnector.sendStartsJSNI(user,state);
+    }
+
+    void translateXML(String result) {
         
-        setState(state);
-    }
-    
-    // per dire all'applet di rifare interamente
-    // la scansione dopo che RESCAN e' stato premuto
-    private void reScanAll() {
-        showStatus("RESCAN");
-        
-        sendRescan();
-    }
-    
-    /**
-     * Metodo che deve chiamare il metodo dell'applet 
-     * letsGo(String username, boolean state)
-     * per mandare user e stato iniziale e avviare il thread
-     */
-    private void sendStarts() {
-    	System.out.println("invio i dati: "+user);
-    	sendStartsJSNI(user,state);
-    	System.out.println("Inviati!");
-    }
-    
-    private native void sendStartsJSNI( String user, boolean state )/*-{
-    	var t = $doc.getElementById('netmus_applet');
-    	t.letsGO(user,state);
-    }-*/;
-    
-    private native void setState( boolean state )/*-{
-        var t = $doc.getElementById('netmus_applet');
-        t.setState(state);
-    }-*/;
-    
-    private native void sendRescan()/*-{
-        var t = $doc.getElementById('netmus_applet');
-        t.rescanAll();
-    }-*/;
-    
-    private native void showChooser()/*-{
-        var t = $doc.getElementById('netmus_applet');
-        t.showChooser();
-    }-*/;
-    
-    private void scanningStatus(int actual, int total){
-    	//aggiornare la grafica con le nuove info
-        showStatus(actual+"/"+total);
-    }
-    
-    private void showStatus(String status){
-    	//modifica le informazioni di stato sulla grafica
-    	System.out.println("Stato: "+status);
-    	this.status.setText("Stato: "+status);
-    }
-    
-    private void translateXML(String result) {
-        
-        System.out.println("Dati XML arrivati: \n"+result);
+        AppletBarView.showStatus(constants.xmlParsing());
         List<SongDTO> new_songs = translator.XMLToDTO(result);
-        if (new_songs == null){showStatus("Errore nel parsing XML");}
-        System.out.println("parsing riuscito");
+        if (new_songs == null)
+            AppletBarView.showStatus(constants.xmlParsingErroor());
         
-        this.status.setText("Dati XML arrivati");
         AsyncCallback<Void> callback = new AsyncCallback<Void>() {
             @Override
             public void onFailure(Throwable caught) {
-                showStatus("Sending Error");
-                System.out.println("KO");
+                AppletBarView.showStatus(constants.sendingErroor());
                 System.err.println(caught.toString());
             }
             @Override
             public void onSuccess(Void result) {
-                showStatus("Sent to Server");
-                System.out.println("OK");
+                AppletBarView.showStatus(constants.sentToServer());
                 client_factory.getEventBus().fireEvent(new DeviceScannedEvent());
             }
         };
         
-        libraryService.sendUserNewMusic(original_user, new_songs, callback);
-        System.out.println("Dati XML spediti");
+        libraryService.sendUserNewMusic(user, new_songs, callback);
+        AppletBarView.showStatus(constants.pleaseWait());
     }
-    
-    private void rescanNotVisible() {
-        rescan.setVisible(false);
-    }
-    private void rescanVisible() {
-        rescan.setVisible(true);
-    }
-    
-    // metodo per pubblicare le funzioni native di linking
-    private native void makeNativeFunction(AppletBar x)/*-{
-    $wnd.getStarts = function () {
-    x.@it.unipd.netmus.client.applet.AppletBar::sendStarts()();
-    };
-    $wnd.scanResult = function (result) {
-    x.@it.unipd.netmus.client.applet.AppletBar::translateXML(Ljava/lang/String;)(result);
-    };
-    $wnd.scanStatus = function (actual, total) {
-    x.@it.unipd.netmus.client.applet.AppletBar::scanningStatus(II)(actual, total);
-    };
-    $wnd.showStatus = function (s) {
-    x.@it.unipd.netmus.client.applet.AppletBar::showStatus(Ljava/lang/String;)(s);
-    };
-    $wnd.rescanVisible = function () {
-    x.@it.unipd.netmus.client.applet.AppletBar::rescanVisible()();
-    };
-    $wnd.rescanNotVisible = function () {
-    x.@it.unipd.netmus.client.applet.AppletBar::rescanNotVisible()();
-    };
-    }-*/;
-    // mancano parametri sopra in ingresso Stringa XML da applet
+
 }
