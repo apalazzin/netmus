19e71fda1eb8d94ad8fc0da8b2969088a9b10f77
giovytr <giovytr@trezzi.net>
2011-03-08 14:15:01 +0100

    FIX: Risolto il tempo di latenza x il caricamento del CoverMode.

---
+++ src/it/unipd/netmus/shared/MusicLibraryDTO.java
@@ -0,0 +1,78 @@
+package it.unipd.netmus.shared;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Nome: MusicLibraryDTO.java 
+ * Autore: VT.G 
+ * Licenza: GNU GPL v3 
+ * Data Creazione: 13 Febbraio 2011
+ * 
+ * 
+ * Tipo, obiettivo e funzione del componente:
+ * 
+ * Come tutte le altri classi di questo package questa classe permette lo
+ * scambio di informazioni tra client e server all'interno delle chiamate RPC.
+ * Contiene le informazioni relative ad un catalogo multimediale compresa la
+ * lista di canzoni di cui è composto. Le canzoni presenti nella lista sono
+ * incapsulate in oggetti di tipo SongDTO.
+ * 
+ * 
+ */
+@SuppressWarnings("serial")
+public class MusicLibraryDTO implements Serializable {
+
+    private UserDTO owner;
+
+    private List<SongDTO> songs;
+
+    private List<String> playlists;
+
+    public MusicLibraryDTO() {
+        this.owner = new UserDTO();
+        this.songs = new ArrayList<SongDTO>();
+    }
+
+    public MusicLibraryDTO(UserDTO user, List<SongDTO> songs,
+            List<String> playlists) {
+        setOwner(user);
+        setSongs(songs);
+        setPlaylists(playlists);
+    }
+
+    public void addPlaylist(String playlist_name) {
+        playlists.add(playlist_name);
+    }
+
+    public UserDTO getOwner() {
+        return owner;
+    }
+
+    public List<String> getPlaylists() {
+        return playlists;
+    }
+
+    public List<SongDTO> getSongs() {
+        return songs;
+    }
+
+    public void removePlaylist(String playlist_name) {
+        if (playlists.contains(playlist_name)) {
+            playlists.remove(playlist_name);
+        }
+    }
+
+    public void setOwner(UserDTO owner) {
+        this.owner = owner;
+    }
+
+    public void setPlaylists(List<String> playlists) {
+        this.playlists = playlists;
+    }
+
+    public void setSongs(List<SongDTO> songs) {
+        this.songs = songs;
+    }
+}
