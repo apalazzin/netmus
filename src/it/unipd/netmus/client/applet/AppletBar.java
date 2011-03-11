40a880b716be376b98567bc901198bfd3fd6efb4
giovytr <giovytr@trezzi.net>
2011-03-11 03:30:29 +0100

    FIX: sfruttate le cover e youtube nel summary sul client x migliorare i metodi il CoverMode e il metodo youTubePlay() in modo da ottimizzare i caricamenti e le informazioni gia' a disposizione.

---
+++ src/it/unipd/netmus/client/applet/AppletBar.java
@@ -0,0 +1,204 @@
+package it.unipd.netmus.client.applet;
+
+import it.unipd.netmus.client.ClientFactory;
+import it.unipd.netmus.client.event.DeviceScannedEvent;
+import it.unipd.netmus.client.service.LibraryService;
+import it.unipd.netmus.client.service.LibraryServiceAsync;
+import it.unipd.netmus.client.service.UserService;
+import it.unipd.netmus.client.service.UserServiceAsync;
+import it.unipd.netmus.shared.SongDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Nome: AppletBar.java 
+ * Autore: VT.G 
+ * Licenza: GNU GPL v3 
+ * Data Creazione: 18 Febbraio 2011
+ */
+public class AppletBar {
+
+    static AppletBar APPLET_BAR = null; // oggetto istanza statico per SINGLETON
+    private boolean visible = false; // stato di visibilita' della barra
+    private String user; // identificativo utente loggato
+    private boolean state = true; // stato applet alla creazione
+    private ClientFactory client_factory = GWT.create(ClientFactory.class);
+    private LibraryServiceAsync library_service = GWT
+            .create(LibraryService.class);
+    private UserServiceAsync user_service = GWT
+    .create(UserService.class);
+    private static AppletConstants constants = GWT
+            .create(AppletConstants.class);
+
+    /**
+     * Se non esiste già, crea una nuova istanza di AppletBar e la salva nel
+     * relativo campo statico. Restituisce l'istanza dopo aver aggiornato
+     * l'utente.
+     */
+    public static AppletBar get(String user) {
+        if (APPLET_BAR == null)
+            APPLET_BAR = new AppletBar(user);
+        else
+            APPLET_BAR.setUser(user);
+        return APPLET_BAR;
+    }
+
+    private TranslateDTOXML translator = new TranslateDTOXML();
+
+    /**
+     * costruttore privato
+     */
+    private AppletBar(String user) {
+        this.user = user;
+        // pubblico i metodi nativi per renderli invocabili dall'applet
+        AppletConnector.makeNativeFunction(this);
+        // creo la view dell'applet
+        AppletBarView.createView();
+    }
+
+    /**
+     * Elimina il Menù laterale
+     */
+    public void appletBarOFF() {
+        if (visible) {
+            visible = false;
+            AppletBarView.hideBar();
+        }
+    }
+
+    /**
+     * Crea il menù laterale.
+     */
+    public void appletBarON() {
+        if (!visible) {
+            visible = true;
+            AppletBarView.showBar();
+        }
+    }
+
+    /**
+     * Attiva/disattiva la scansione automatica, e aggiorna view e applet di
+     * conseguenza.
+     */
+    void changeState() {
+        state = !state;
+        if (state)
+            AppletBarView.setOnOffButton(false);
+        else
+            AppletBarView.setOnOffButton(true);
+        AppletConnector.setState(state); // cambio dello stato dentro l'applet
+    }
+
+    // --- METODI PER LA LOGICA DI COMUNICAZIONE TRA VIEW E APPLET ---
+
+    /**
+     * Indica all'applet di riscansionare l'ultimo device.
+     */
+    void reScanAll() {
+        AppletConnector.sendRescan();
+    }
+
+    /**
+     * Inizializza l'applet.
+     */
+    void sendStarts() {
+        AppletConnector.sendStartsJSNI(user, state);
+    }
+
+    /**
+     * Indica all'applet di effettuare una scansione manuale.
+     */
+    void showChooser() {
+        AppletConnector.showChooser();
+    }
+
+    /**
+     * Dall'XML ricevuto dall'applet estrae le canzoni e le invia al server.
+     * 
+     * @param result
+     */
+    void translateXML(String result) {
+
+        AppletBarView.showStatus(constants.pleaseWait());
+        sendMusic(result);   
+    }
+    
+    private void sendMusic(String xml) {
+        
+        List<SongDTO> new_songs = translator.XMLToDTO(xml);
+        
+        if (new_songs == null) {
+            AppletBarView.showStatus(constants.xmlParsingError());
+            return;
+        }
+        
+        library_service.sendUserNewMusic(user, new_songs, new AsyncCallback<Void>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                AppletBarView.showStatus(constants.sendingError());
+                System.err.println(caught.toString());
+            }
+
+            @Override
+            public void onSuccess(Void incomplete) {
+                
+                if (translator.otherChild()) {
+                    AppletBarView.showStatus(String.valueOf((translator.getParsed())));
+                    sendMusic("");
+                }
+                    
+                else {
+                    AppletBarView.showStatus(constants.completionFinish());
+                    client_factory.getEventBus()
+                            .fireEvent(new DeviceScannedEvent());
+                    
+//                    AppletBarView.showStatus(constants.updatingStatistics());
+//                    
+//                    // nuova RPC per far partire l'update delle statistiche
+//                    library_service.updateStatisticFields(user, new AsyncCallback<Void>() {
+//                        @Override
+//                        public void onFailure(Throwable caught) {
+//                        }
+//                        @Override
+//                        public void onSuccess(Void result) {
+//                            AppletBarView.showStatus(constants.completionFinish());
+//                            
+//                            //nuova RPC per aggiornare la lista degli amici successivamente all'aggiornamento
+//                            user_service.findRelatedUsers(user, new AsyncCallback<List<String>>() {
+//                                @Override
+//                                public void onFailure(Throwable caught) {
+//                                }
+//
+//                                @Override
+//                                public void onSuccess(List<String> related_users) {
+//                                    String[] names = new String[related_users.size()];
+//                                    
+//                                    for (int i=0; i<related_users.size(); i++) {
+//                                        names[i] = related_users.get(i);
+//                                    }
+//                                    
+//                                    client_factory.getProfileView().paintFriendlist(names);
+//                                }
+//                                
+//                            });
+//                        }
+//                    });
+                }
+                
+            }
+        });
+        
+    }
+
+    /**
+     * Aggiorna l'attributo user.
+     */
+    private void setUser(String user) {
+        this.user = user;
+    }
+
+}
---
+++ src/it/unipd/netmus/server/UserServiceImpl.java
@@ -0,0 +1,89 @@
+package it.unipd.netmus.server;
+
+import it.unipd.netmus.client.service.UserService;
+import it.unipd.netmus.server.persistent.UserAccount;
+import it.unipd.netmus.server.utils.BCrypt;
+import it.unipd.netmus.shared.UserCompleteDTO;
+
+import java.util.List;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * Nome: UserServiceImpl.java 
+ * Autore: VT.G 
+ * Licenza: GNU GPL v3 
+ * Data Creazione: 13 Febbraio 2011
+ */
+@SuppressWarnings("serial")
+public class UserServiceImpl extends RemoteServiceServlet implements
+        UserService {
+
+    /**
+     * Cancella irreversibilmente l’utente e tutte le sue informazioni dal
+     * Datastore e conseguentemente lo reindirizza alla pagina iniziale di
+     * login. Le canzoni che facevano parte del catalogo non vengono cancellate.
+     */
+    @Override
+    public boolean deleteProfile(String user) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * Salva nell’ UserAccount del Datastore i dati presenti nell’
+     * UserCompleteDTO dato in input sovrascrivendo le informazioni precedenti.
+     * E previsto qui anche il cambio della password.
+     */
+    @Override
+    public boolean editProfile(String user, UserCompleteDTO new_info_user) {
+
+        try {
+            UserAccount current_user = UserAccount.load(user);
+
+            if (!new_info_user.getNewPassword().equals("")) {
+                String passwordHash = BCrypt.hashpw(
+                        new_info_user.getNewPassword(), BCrypt.gensalt());
+                current_user.setPassword(passwordHash);
+            }
+
+            current_user.setNickName(new_info_user.getNickName());
+            current_user.setFirstName(new_info_user.getFirstName());
+            current_user.setLastName(new_info_user.getLastName());
+            current_user.setGender(new_info_user.getGender());
+            current_user.setNationality(new_info_user.getNationality());
+            current_user.setAboutMe(new_info_user.getAboutMe());
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    /**
+     * Cerca nel Datastore i nomi degli utenti il cui catalogo ha proprietà
+     * simili a quello dato in input. I criteri di somiglianza sono dati
+     * dall’artista più ricorrente ed il genere più ascoltato.
+     */
+    @Override
+    public List<String> findRelatedUsers(String user) {
+        UserAccount user_account = UserAccount.load(user);
+        return user_account.findRelatedUsers();
+    }
+
+    /**
+     * Trova nel Datastore l’utente a cui corrisponde l’username dato in input e
+     * ne ritorna le informazioni incapsulate in un DTO. Gli utenti di cui viene
+     * richiesto il caricamento devono essere presenti nel Datastore.
+     */
+    @Override
+    public UserCompleteDTO loadProfile(String user) {
+        UserAccount user_account = UserAccount.load(user);
+        if (user_account != null)
+            return user_account.toUserCompleteDTO();
+        else return null;
+        
+    }
+
+}
