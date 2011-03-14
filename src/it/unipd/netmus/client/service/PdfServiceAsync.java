f6c1425dba57bfc77a273abea046dec0fb98fe95
fede baron 89 <fede.baron.89@gmail.com>
2011-03-14 22:38:28 +0100

    ADD - utils.velocity, GdataManager, PDFLoaderServlet - Spostate le classi necessarie all'esportazione PDF nei package adeguati. Per il corretto utilizzo delle nuove librerie ricordarsi di aggiungere nella Build Path i nuovi pacchetti inseriti in WEB-INF/lib.

--- src/it/unipd/netmus/client/service/PdfServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package it.unipd.netmus.client.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface PdfServiceAsync {
-    void exportPDF(String user, AsyncCallback<String> callback);
-}
