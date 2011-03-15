bbc98b92aa5472209fb481db4a1872c8c0b66b97
fede baron 89 <fede.baron.89@gmail.com>
2011-03-15 14:20:26 +0100

    DEL - MusicLibraryDTO, MusicLibrarySummaryDTO - Rimozione della classe MusicLibraryDTO perch? non utilizzata e rinominazione di MusicLibrarySummaryDTO a MusicLibraryDTO.

--- src/it/unipd/netmus/shared/MusicLibraryDTO.java
+++ src/it/unipd/netmus/shared/MusicLibraryDTO.java
@@ -2,14 +2,15 @@ package it.unipd.netmus.shared;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Nome: MusicLibraryDTO.java 
+ * Nome: MusicLibrarySummaryDTO.java 
  * Autore: VT.G 
  * Licenza: GNU GPL v3 
  * Data Creazione: 13 Febbraio 2011
- * 
  * 
  * Tipo, obiettivo e funzione del componente:
  * 
@@ -17,47 +18,60 @@ import java.util.List;
  * scambio di informazioni tra client e server all'interno delle chiamate RPC.
  * Contiene le informazioni relative ad un catalogo multimediale compresa la
  * lista di canzoni di cui è composto. Le canzoni presenti nella lista sono
- * incapsulate in oggetti di tipo SongDTO.
- * 
+ * incapsulate in oggetti di tipo SongSummaryDTO e contengono solamente le
+ * infomrazioni basilari quali artista e titolo.
  * 
  */
 @SuppressWarnings("serial")
 public class MusicLibraryDTO implements Serializable {
 
-    private List<SongDTO> songs;
+    private Map<String, SongSummaryDTO> songs;
 
     private List<String> playlists;
     
     private String preferred_artist;
 
-    private String preferred_genre;
+    private String most_popular_song;
+    
+    private String most_popular_song_for_this_user;
 
     public MusicLibraryDTO() {
-        this.songs = new ArrayList<SongDTO>();
+        this.songs = new HashMap<String, SongSummaryDTO>();
+        this.playlists = new ArrayList<String>();
         this.preferred_artist = "";
-        this.preferred_genre = "";
+        this.most_popular_song = "";
+        this.most_popular_song_for_this_user = "";
     }
 
-    public MusicLibraryDTO(List<SongDTO> songs,
+    public MusicLibraryDTO(Map<String, SongSummaryDTO> m,
             List<String> playlists) {
-        setSongs(songs);
+        setSongs(m);
         setPlaylists(playlists);
         this.preferred_artist = "";
-        this.preferred_genre = "";
+        this.most_popular_song = "";
+        this.most_popular_song_for_this_user = "";
+    }
+    
+    public Map<String, SongSummaryDTO> getSongs() {
+        return songs;
     }
 
+    private void setSongs(Map<String, SongSummaryDTO> songs) {
+        this.songs = songs;
+    }
+    
     public void addPlaylist(String playlist_name) {
         playlists.add(playlist_name);
+    }
+
+    public int getLibrarySize() {
+        return songs.size();
     }
 
     public List<String> getPlaylists() {
         return playlists;
     }
-
-    public List<SongDTO> getSongs() {
-        return songs;
-    }
-
+    
     public void removePlaylist(String playlist_name) {
         if (playlists.contains(playlist_name)) {
             playlists.remove(playlist_name);
@@ -68,10 +82,6 @@ public class MusicLibraryDTO implements Serializable {
         this.playlists = playlists;
     }
 
-    public void setSongs(List<SongDTO> songs) {
-        this.songs = songs;
-    }
-
     public void setPreferred_artist(String preferred_artist) {
         this.preferred_artist = preferred_artist;
     }
@@ -80,11 +90,20 @@ public class MusicLibraryDTO implements Serializable {
         return preferred_artist;
     }
 
-    public void setPreferred_genre(String preferred_genre) {
-        this.preferred_genre = preferred_genre;
+    public void setMostPopularSong(String most_popular_song) {
+        this.most_popular_song = most_popular_song;
     }
 
-    public String getPreferred_genre() {
-        return preferred_genre;
+    public String getMostPopularSong() {
+        return most_popular_song;
+    }
+
+    public void setMostPopularSongForThisUser(
+            String most_popular_song_for_this_user) {
+        this.most_popular_song_for_this_user = most_popular_song_for_this_user;
+    }
+
+    public String getMostPopularSongForThisUser() {
+        return most_popular_song_for_this_user;
     }
 }
