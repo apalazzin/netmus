30c803b4208d36215945920fcfc0e9312b3372a6
giovytr <giovytr@trezzi.net>
2011-03-16 12:22:39 +0100

    FIX: player youtube che a volte restava aperto dopo la chiusura. FIX: con il tasto backword ora la canzone evidenziata in play resta una e nn tutte le precedenti come prima.

---
+++ src/it/unipd/netmus/server/servlet/LoginSuperServlet.java
@@ -0,0 +1,30 @@
+package it.unipd.netmus.server.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Nome: LoginSuperServlet.java 
+ * Autore: VT.G 
+ * Licenza: GNU GPL v3 
+ * Data Creazione: 17 Febbraio 2011
+ * 
+ */
+@SuppressWarnings("serial")
+public abstract class LoginSuperServlet extends HttpServlet {
+
+    public LoginSuperServlet() {
+        super();
+    }
+
+    /**
+     * Crea l’indirizzo richiedente, per login.
+     */
+    protected String buildCallBackURL(HttpServletRequest request) {
+        StringBuffer requestURL = request.getRequestURL();
+        String callbackURL = requestURL.toString();
+        callbackURL += "callback";
+        return callbackURL;
+    }
+
+}
