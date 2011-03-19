8a1d7a4c1b44c88fc593fede36d78bcca9cee3ed
giovytr <giovytr@trezzi.net>
2011-03-19 15:55:56 +0100

    FIX: resetView(0

---
+++ src/it/unipd/netmus/client/ui/MyConstants.java
@@ -0,0 +1,88 @@
+package it.unipd.netmus.client.ui;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * Nome: MyConstants.java
+ * Autore:  VT.G
+ * Licenza: GNU GPL v3
+ * Data Creazione: 19 Febbraio 2011
+*/
+public interface MyConstants extends Constants {
+	  
+	String loginLabel();
+	String registerLabel();
+	String registerSwitchLabel();
+	String loginSwitchLabel();
+	String accountGoogle();  
+	String accountNetmus();  
+	String passwordCheck();
+	String errorPassword();
+	String errorCPassword();
+	String errorEmail();
+	String infoUserInsertDb();
+	String infoUserUsato();
+	String infoUserAlreadyDb();
+	String infoCorrectLogin();
+	String infoLoginIncorrect();
+	String databaseErrorGeneric();
+    String accountGoogleLogin();
+	String leavingProfilePage();
+	String confirmDelete();
+	String confirmDeleteProfile();
+	String yes();
+	String no();
+	String downloadPDF();
+	
+	String addPlaylistError();
+	String addSongToPlaylistError();
+	String removePlaylistError();
+	String deleteSongError();
+	String editProfileError();
+	String findRelatedUsersError();
+	String getPlaylistError();
+	String getProfileViewError();
+	String getSongDTOError();
+	String rateSongError();
+	String storeStatisticsError();
+	String removeSongFromPlaylistError();
+	String loadProfileError();
+	String generatePDFError();
+	String deleteProfileError();
+	String noVoteForThisUserError();
+	String noVoteError();
+	String voteNullSongError();
+	String moveSongError();
+	
+	String friendTitolo();
+	String info();
+	String titleSong();
+	String artistSong();
+	String albumSong();
+	String genreSong();
+	String yearSong();
+	String composerSong();
+	String trackSong();
+	String ratingSong();
+	String vote();
+	String export();
+	String statisticL();
+	String editProfileCheck();
+	String name();
+	String surname();
+	String nation();
+	String sex();
+	String editProfileLabelCpassword();
+	String aboutMe();
+	String statNetmus();
+	String statNum();
+	String statPref();
+	String statSongPref();
+	String statPrefNetmus();
+	String statClose();
+	String artist();
+	String title();
+	String notYoutube();
+	String unknownAlbum();
+	
+	}
